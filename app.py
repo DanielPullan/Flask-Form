cd3ebdc555fe963054a167590acdc26ff327e73e
Dan Pullan <dan@3264.uk>
2020-01-23 16:56:19 +0000

    beginning of login
    working on the very start of login script.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template #import main Flask class and request object
+from flask import Flask, request, render_template, make_response
 import pymysql
 from config import where_did_you_come_from, the_greatest_username_ever, the_most_secure_password_ever
 
@@ -61,14 +61,66 @@ def form_example():
 			cur.execute("INSERT INTO users (name,lastname,email) VALUES (%s,%s,%s);", (name, lastname,email))
 			cur.close()
 
-			#return '''<h1>Data Submitted</h1>
-			#		  <p>Values: {}{}{}</p>'''.format(name, lastname, email)
-
 			return render_template("form-success.html", name=name, lastname=lastname, email=email)
 		except:
 			return render_template("form-failure.html")
 
 	return render_template("form-example.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		# get login details from form
+		username = request.form['username']
+		password = request.form['password']
+		pin = request.form['pin']
+
+		# mysql query to check if details exist/are correct
+		cur = conn.cursor()
+		cur.execute("SELECT * FROM login where username = %s;", (username))
+		cur.close()
+		results = cur.fetchone()
+
+	#TODO: blah stuff. get the data from the data in blah to become variables and then do the whole if the form password matches the database one, create the cookie and shit
+
+		submittedusername = results[1]
+		submittedpassword = results[2]
+		submittedpin = results[3]
+
+		if username == submittedusername:
+			print("username matches")
+		else:
+			return "fail"
+
+		if password == submittedpassword:
+			print("password matches")
+		else:
+			return "fail"
+
+		if pin == submittedpin:
+			print("pin matches, we good in the hood")
+		else: 
+			return "fail"
+
+		return result
+
+		# if successful, set the cookie and go to successful login
+
+		# if failed, don't set the cookie, go to sad cat
+		#return render_template("form-success.html", name=name, lastname=lastname, email=email)
+	return render_template("login.html")
+	
+
+@app.route('/set')
+def setcookie():
+	resp = make_response('Setting cookie!')
+	resp.set_cookie('genitals', 'penis')
+	return resp
+
+@app.route('/get')
+def getcookie():
+	framework = request.cookies.get('genitals')
+	return framework
 
 if __name__ == "__main__":
 	app.run(debug=True)
