7b9e5ec44cb588fab425ab64b0fe498f75b9b7a3
Dan Pullan <dan@3264.uk>
2020-01-30 10:47:03 +0000

    Reporting works 🎆

--- app.py
+++ app.py
@@ -213,61 +213,161 @@ def add_location():
 
 @app.route('/get-report')
 def get_report():
+	cookie = str(request.cookies.get('OhCanada'))
+	if cookie == "GreenAndPleasantLand":
+		current_agent = str(request.cookies.get('User'))
 
-	current_agent = str(request.cookies.get('User'))
+		cur = conn.cursor()
+		cur.execute("SELECT * from agents where name = %s", (current_agent))
+		agents_result = cur.fetchone()[0:12]
+		#number, name, lastname, phonenumber, email, business, address_line1, address_line2, town, county, postcode, siccode = cur.fetchall()
+		cur.close()
 
-	cur = conn.cursor()
-	cur.execute("SELECT customer_name FROM waste_transfer where agent_name = %s ORDER BY number DESC LIMIT 1", (current_agent))
-	customer = str(cur.fetchone()[0])
-	cur.close()
+		# get customer name
+		cur = conn.cursor()
+		cur.execute("SELECT customer_name FROM waste_transfer where agent_name = %s ORDER BY number DESC LIMIT 1", (current_agent))
+		customer = str(cur.fetchone()[0])
+		cur.close()
 
-	cur = conn.cursor()
-	cur.execute("SELECT * from transferors where name = %s", (customer))
-	transgerors_result = cur.fetchone()[0:12]
-	#number, name, lastname, phonenumber, email, business, address_line1, address_line2, town, county, postcode, siccode = cur.fetchall()
-	cur.close()
+		# get location name
+		cur = conn.cursor()
+		cur.execute("SELECT collection_point from waste_transfer where agent_name = %s ORDER BY number DESC LIMIT 1", (current_agent))
+		job_location = cur.fetchone()[0]
+		cur.close()
 
-	transferors_name = str(transferors_result[1])
-	transferors_lastname = str(transferors_result[2])
-	transferors_phonenumber = str(transferors_result[3])
-	transferors_email = str(transferors_result[4])
-	transferors_business = str(transferors_result[5])
-	transferors_address_line1 = str(transferors_result[6])
-	transferors_address_line2 = str(transferors_result[7])
-	transferors_town = str(transferors_result[8])
-	transferors_county = str(transferors_result[9])
-	transferors_postcode = str(transferors_result[10])
-	transferors_siccode = str(transferors_result[11])
+		# get location details
+		cur = conn.cursor()
+		cur.execute("SELECT * from collection_point where name = %s", (job_location))
+		collection_result = cur.fetchone()[0:8]
+		#number, name, lastname, phonenumber, email, business, address_line1, address_line2, town, county, postcode, siccode = cur.fetchall()
+		cur.close()
 
-	cur = conn.cursor()
-	cur.execute("SELECT * from agents where name = %s", (agent_name))
-	agents_result = cur.fetchone()[0:12]
-	#number, name, lastname, phonenumber, email, business, address_line1, address_line2, town, county, postcode, siccode = cur.fetchall()
-	cur.close()
+		cur = conn.cursor()
+		cur.execute("SELECT * from transferors where name = %s", (customer))
+		transferors_result = cur.fetchone()[0:12]
+		#number, name, lastname, phonenumber, email, business, address_line1, address_line2, town, county, postcode, siccode = cur.fetchall()
+		cur.close()
 
-	agents_name = str(agents_result[1])
-	agents_lastname = str(agents_result[2])
-	agents_phonenumber = str(agents_result[3])
-	agents_email = str(agents_result[4])
-	agents_business = str(agents_result[5])
-	agents_address_line1 = str(agents_result[6])
-	agents_address_line2 = str(agents_result[7])
-	agents_town = str(agents_result[8])
-	agents_county = str(agents_result[9])
-	agents_postcode = str(agents_result[10])
-	agents_siccode = str(agents_result[11])
+		collection_name = str(collection_result[1])
+		collection_company = str(collection_result[2])
+		collection_address_line1 = str(collection_result[3])
+		collection_address_line2 = str(collection_result[4])
+		collection_town = str(collection_result[5])
+		collection_county = str(collection_result[6])
+		collection_postcode = str(collection_result[7])
 
+		transferors_name = str(transferors_result[1])
+		transferors_lastname = str(transferors_result[2])
+		transferors_phonenumber = str(transferors_result[3])
+		transferors_email = str(transferors_result[4])
+		transferors_business = str(transferors_result[5])
+		transferors_address_line1 = str(transferors_result[6])
+		transferors_address_line2 = str(transferors_result[7])
+		transferors_town = str(transferors_result[8])
+		transferors_county = str(transferors_result[9])
+		transferors_postcode = str(transferors_result[10])
+		transferors_siccode = str(transferors_result[11])
 
+		agents_name = str(agents_result[1])
+		agents_lastname = str(agents_result[2])
+		agents_phonenumber = str(agents_result[3])
+		agents_email = str(agents_result[4])
+		agents_business = str(agents_result[5])
+		agents_address_line1 = str(agents_result[6])
+		agents_address_line2 = str(agents_result[7])
+		agents_town = str(agents_result[8])
+		agents_county = str(agents_result[9])
+		agents_postcode = str(agents_result[10])
+		agents_siccode = str(agents_result[11])
 
+		# Note to future Dan. the above works. you need to create a template for a report. Look at the dead trees on the desk for ideas. This was a massive pain to get working right.
 
-	# Note to future Dan. the above works. you need to create a template for a report. Look at the dead trees on the desk for ideas. This was a massive pain to get working right.
+		title="Report"
+		
 
-	title="Report"
-	
+		return render_template("report.html", title=title, transferors_name=transferors_name, transferors_lastname=transferors_lastname, transferors_phonenumber=transferors_phonenumber, transferors_email=transferors_email, transferors_business=transferors_business, transferors_address_line1=transferors_address_line1, transferors_address_line2=transferors_address_line2, transferors_town=transferors_town, transferors_county=transferors_county, transferors_postcode=transferors_postcode, transferors_siccode=transferors_siccode, agent_name=agents_name, agents_lastname=agents_lastname, agents_phonenumber=agents_phonenumber, agents_email=agents_email, agents_business=agents_business, agents_address_line1=agents_address_line1, agents_address_line2=agents_address_line2, agents_town=agents_town, agents_county=agents_county, agents_postcode=agents_postcode, agents_siccode=agents_siccode, collection_name=collection_name, collection_company=collection_company, collection_address_line1=collection_address_line1, collection_address_line2=collection_address_line2, collection_town=collection_town, collection_county=collection_county, collection_postcode=collection_postcode)
+	else:
+		res = make_response(redirect('/stare'))
+		return res
 
-	return render_template("report.html", title=title, transferors_name=transferors_name, transferors_lastname=transferors_lastname, transferors_phonenumber=transferors_phonenumber, transferors_email=transferors_email, transferors_business=transferors_business, transferors_address_line1=transferors_address_line1, transferors_address_line2=transferors_address_line2, transferors_town=transferors_town, transferors_county=transferors_county, transferors_postcode=transferors_postcode, transferors_siccode=transferors_siccode)
+@app.route('/print-report')
+def print_report():
+	cookie = str(request.cookies.get('OhCanada'))
+	if cookie == "GreenAndPleasantLand":
+		current_agent = str(request.cookies.get('User'))
 
+		cur = conn.cursor()
+		cur.execute("SELECT * from agents where name = %s", (current_agent))
+		agents_result = cur.fetchone()[0:12]
+		#number, name, lastname, phonenumber, email, business, address_line1, address_line2, town, county, postcode, siccode = cur.fetchall()
+		cur.close()
 
+		# get customer name
+		cur = conn.cursor()
+		cur.execute("SELECT customer_name FROM waste_transfer where agent_name = %s ORDER BY number DESC LIMIT 1", (current_agent))
+		customer = str(cur.fetchone()[0])
+		cur.close()
+
+		# get location name
+		cur = conn.cursor()
+		cur.execute("SELECT collection_point from waste_transfer where agent_name = %s ORDER BY number DESC LIMIT 1", (current_agent))
+		job_location = cur.fetchone()[0]
+		cur.close()
+
+		# get location details
+		cur = conn.cursor()
+		cur.execute("SELECT * from collection_point where name = %s", (job_location))
+		collection_result = cur.fetchone()[0:8]
+		#number, name, lastname, phonenumber, email, business, address_line1, address_line2, town, county, postcode, siccode = cur.fetchall()
+		cur.close()
+
+		cur = conn.cursor()
+		cur.execute("SELECT * from transferors where name = %s", (customer))
+		transferors_result = cur.fetchone()[0:12]
+		#number, name, lastname, phonenumber, email, business, address_line1, address_line2, town, county, postcode, siccode = cur.fetchall()
+		cur.close()
+
+		collection_name = str(collection_result[1])
+		collection_company = str(collection_result[2])
+		collection_address_line1 = str(collection_result[3])
+		collection_address_line2 = str(collection_result[4])
+		collection_town = str(collection_result[5])
+		collection_county = str(collection_result[6])
+		collection_postcode = str(collection_result[7])
+
+		transferors_name = str(transferors_result[1])
+		transferors_lastname = str(transferors_result[2])
+		transferors_phonenumber = str(transferors_result[3])
+		transferors_email = str(transferors_result[4])
+		transferors_business = str(transferors_result[5])
+		transferors_address_line1 = str(transferors_result[6])
+		transferors_address_line2 = str(transferors_result[7])
+		transferors_town = str(transferors_result[8])
+		transferors_county = str(transferors_result[9])
+		transferors_postcode = str(transferors_result[10])
+		transferors_siccode = str(transferors_result[11])
+
+		agents_name = str(agents_result[1])
+		agents_lastname = str(agents_result[2])
+		agents_phonenumber = str(agents_result[3])
+		agents_email = str(agents_result[4])
+		agents_business = str(agents_result[5])
+		agents_address_line1 = str(agents_result[6])
+		agents_address_line2 = str(agents_result[7])
+		agents_town = str(agents_result[8])
+		agents_county = str(agents_result[9])
+		agents_postcode = str(agents_result[10])
+		agents_siccode = str(agents_result[11])
+
+		# Note to future Dan. the above works. you need to create a template for a report. Look at the dead trees on the desk for ideas. This was a massive pain to get working right.
+
+		title="Report"
+		
+
+		return render_template("print-report.html", title=title, transferors_name=transferors_name, transferors_lastname=transferors_lastname, transferors_phonenumber=transferors_phonenumber, transferors_email=transferors_email, transferors_business=transferors_business, transferors_address_line1=transferors_address_line1, transferors_address_line2=transferors_address_line2, transferors_town=transferors_town, transferors_county=transferors_county, transferors_postcode=transferors_postcode, transferors_siccode=transferors_siccode, agent_name=agents_name, agents_lastname=agents_lastname, agents_phonenumber=agents_phonenumber, agents_email=agents_email, agents_business=agents_business, agents_address_line1=agents_address_line1, agents_address_line2=agents_address_line2, agents_town=agents_town, agents_county=agents_county, agents_postcode=agents_postcode, agents_siccode=agents_siccode, collection_name=collection_name, collection_company=collection_company, collection_address_line1=collection_address_line1, collection_address_line2=collection_address_line2, collection_town=collection_town, collection_county=collection_county, collection_postcode=collection_postcode)
+	else:
+		res = make_response(redirect('/stare'))
+		return res
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
